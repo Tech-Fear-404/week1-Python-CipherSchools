6b791da16c2a8696be740f09816e0b335d68d65e
Tech-Fear-404 <118983448+Tech-Fear-404@users.noreply.github.com>
2022-11-26 13:44:36 +0530

    Add files via upload

---
+++ Class12-Escape Sequence as Normal Text-CipherSchools.py
@@ -0,0 +1,9 @@
+# output lineA \n lineB
+print(" lineA \\n lineB")
+print(" lineA \\t lineB")
+print("this is 4 backslash \\\\\\\\")
+# output \" \'
+print("\\\" \\\' ")
+# \' - '
+# \\ - \
+# \\\' - \'
