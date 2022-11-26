92068efb57541c44a00d2bfe30d51d502341b035
Tech-Fear-404 <118983448+Tech-Fear-404@users.noreply.github.com>
2022-11-26 18:46:20 +0530

    Delete Class14-Exercise Based on escape sequences-CipherSchools.py

--- Class14-Exercise Based on escape sequences-CipherSchools.py
+++
@@ -1,8 +0,0 @@
-# this is double backslash \\
-print("this is double backslash \\\\")
-# these are /\/\/\/\/\ mountains
-print("these are /\\/\\/\\/\\/\\ mountains")
-#he is    awesome (use escape sequence insted of manual spaces)
-print(" he is\t awesome")
-# \" \n \t \' as an output
-print("\\\" \\n \\t \\\' ")
