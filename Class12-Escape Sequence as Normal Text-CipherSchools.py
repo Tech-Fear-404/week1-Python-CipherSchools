4bcb8dfdc3665a71c87adb228f7d15d411f5d5f6
Tech-Fear-404 <118983448+Tech-Fear-404@users.noreply.github.com>
2022-11-26 18:46:11 +0530

    Delete Class12-Escape Sequence as Normal Text-CipherSchools.py

--- Class12-Escape Sequence as Normal Text-CipherSchools.py
+++
@@ -1,9 +0,0 @@
-# output lineA \n lineB
-print(" lineA \\n lineB")
-print(" lineA \\t lineB")
-print("this is 4 backslash \\\\\\\\")
-# output \" \'
-print("\\\" \\\' ")
-# \' - '
-# \\ - \
-# \\\' - \'
